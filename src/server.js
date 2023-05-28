139407e7eb926760896d7ff76ab9188b4cd3954b
Diego Reis <diegoreso@me.com>
2023-05-28 20:51:48 -0300

    create api post route

--- src/server.js
+++ src/server.js
@@ -11,9 +11,8 @@ const app = express()
 //conexao com banco de dados
 db.connect()//
 
-
-//habilita server para receber dados via post (formulario)
-app.use(express.urlencoded({extended:true}))
+//habilita server para receber dados json
+app.use(express.json())
 
 
 //definindo as rotasrotas
